2ef46c0596287c90489b8223bdcc80fed30f5631
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-10-27 19:58:33 +0200

    крок 2 виконано

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export default function setupServer() {
+  const app = express();
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}
