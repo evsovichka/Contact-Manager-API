567ab4ee472762308d06001fb853b5aed5642a61
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-11-02 20:24:23 +0200

    крок 1 і 2

---
+++ src/routers/contacts.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+  getContactsController,
+  getContactByIdController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -23,41 +25,11 @@ export default function setupServer() {
     res.json({ message: 'Home Page' });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('/contacts', contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
